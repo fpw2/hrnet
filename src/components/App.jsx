3e51a1e5c7746b28ed8223682a05e4cfa9f21455
fpw2 <fabien.pergaud@gmail.com>
2022-11-23 20:10:19 +0100

    rd

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,4 +1,4 @@
-import { BrowserRouter, Routes, Route, Navigate } from "react-router-dom"
+import { BrowserRouter, Routes, Route } from "react-router-dom"
 import Container from "./layout/Container"
 import Header from "./layout/Header"
 import Home from "./home"
